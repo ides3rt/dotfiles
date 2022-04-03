bd84e889cab5a24940f12ac6352d806cbe4c5074
iDes3rt <ides3rt@tutanota.com>
2022-04-03 19:42:10 +0000

    Add forgotten keybinds.

--- src/.config/nvim/lua/keymaps.lua
+++ src/.config/nvim/lua/keymaps.lua
@@ -10,18 +10,20 @@ end
 map('', 'j', 'gj')
 map('', 'k', 'gk')
 
-map('n', 'Q', '<Nop>')
-map('n', 'gQ', '<Nop>')
-map('n', 'q:', '<Nop>')
-
-map('n', 'Y', 'y$')
-
-map('n', '<leader>ic', ':set ignorecase!<CR>')
-map('n', '<leader>sw', ':set wrap! linebreak!<CR>')
-
 map('', '<leader>ac', ':center<CR>')
 map('', '<leader>ar', ':right<CR>')
 map('', '<leader>al', ':left<CR>')
 
 map('n', '<leader>nn', ':next<CR>')
 map('n', '<leader>pp', ':prev<CR>')
+
+map('n', '<leader>ic', ':set ignorecase!<CR>')
+map('n', '<leader>ls', ':set list!<CR>')
+map('n', '<leader>sw', ':set wrap! linebreak!<CR>')
+map('n', '<leader><space>', ':set noh<CR>')
+
+map('n', 'Y', 'y$')
+
+map('n', 'Q', '<Nop>')
+map('n', 'gQ', '<Nop>')
+map('n', 'q:', '<Nop>')
